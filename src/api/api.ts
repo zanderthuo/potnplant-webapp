c66d4bafeb63e2f93482fdc6d20ccaf5a9c48663
zanderthuo <thuozander@gmail.com>
2026-06-18 22:05:20 +0300

    feat:new api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://potnplant-backend.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://apiv1.potnplant.co.ke",
   // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
